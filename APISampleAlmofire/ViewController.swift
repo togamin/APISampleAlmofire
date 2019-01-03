c4afee7899234e0a1e4fa998b5b2d89c2466ade9
Togami Yuki <togaminnnn@gmail.com>
2019-01-03 09:54:50 +0800

    first commit

---
+++ APISampleAlmofire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APISampleAlmofire
+//
+//  Created by Togami Yuki on 2019/01/03.
+//  Copyright © 2019 Togami Yuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
